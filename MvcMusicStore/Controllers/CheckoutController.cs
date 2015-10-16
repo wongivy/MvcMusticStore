3ff0c656c0f631ff8d03badbfbeedb17c7f3e1e1
unknown <IvyW@W7-IVYW-VM.paraport.com>
2015-10-16 16:08:07 -0700

    Finished Initial Tutorial

---
+++ MvcMusicStore/Controllers/CheckoutController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcMusicStore.Models;
+
+namespace MvcMusicStore.Controllers
+{
+    public class CheckoutController : Controller
+    {
+        MusicStoreEntities storeDB = new MusicStoreEntities();
+        private const string PromoCode = "Free";
+        public ActionResult AddressAndPayment()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddressAndPayment(FormCollection values)
+        {
+            var order = new Order();
+            TryUpdateModel(order);
+
+            try
+            {
+                if (string.Equals(values["PromoCode"], PromoCode,
+                    StringComparison.OrdinalIgnoreCase) == false)
+                {
+                    return View(order);
+                }
+                else
+                {
+                    order.OrderDate = DateTime.Now;
+
+                    //Save Order
+                    storeDB.Orders.Add(order);
+                    storeDB.SaveChanges();
+                    //Process the order
+                    var cart = ShoppingCart.GetCart(HttpContext);
+                    cart.CreateOrder(order);
+                    return RedirectToAction("Complete", new { id = order.OrderId });
+                }
+            }
+            catch
+            {
+                //Invalid - redisplay with errors
+                return View(order);
+            }
+        }
+
+        public ActionResult Complete(int id)
+        {
+            // Validate customer owns this order
+            bool isValid = storeDB.Orders.Any(
+                o => o.OrderId == id);
+            if (isValid)
+            {
+                return View(id);
+            }
+            else
+            {
+                return View("Error");
+            }
+        }
+    }
+}
---
+++ MvcMusicStore/Controllers/ShoppingCartController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcMusicStore.Models;
+using MvcMusicStore.ViewModels;
+
+namespace MvcMusicStore.Controllers
+{
+    public class ShoppingCartController : Controller
+    {
+        MusicStoreEntities storeDB = new MusicStoreEntities();
+        // GET: ShoppingCart
+        public ActionResult Index()
+        {
+            var cart = ShoppingCart.GetCart(this.HttpContext);
+            var viewModel = new ShoppingCartViewModel
+            {
+                CartItems = cart.GetCartItems(),
+                CartTotal = cart.GetTotal()
+            };
+            return View(viewModel);
+        }
+
+
+        public ActionResult AddtoCart(int id)
+        {
+            var addedAlbum = storeDB.Albums
+              .Single(album => album.AlbumId == id);
+            var cart = ShoppingCart.GetCart(this.HttpContext);
+            cart.AddToCart(addedAlbum);
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public ActionResult RemoveFromCart(int id)
+        {
+            var cart = ShoppingCart.GetCart(this.HttpContext);
+            string albumName = storeDB.Carts
+                .Single(item => item.RecordId == id).Album.Title;
+            int itemCount = cart.RemoveFromCart(id);
+            var results = new ShoppingCartRemoveViewModel
+            {
+                Message = Server.HtmlEncode(albumName) +
+                    " has been removed from your shopping cart.",
+                CartTotal = cart.GetTotal(),
+                CartCount = cart.GetCount(),
+                ItemCount = itemCount,
+                DeleteId = id
+            };
+            return Json(results);
+        }
+
+        [ChildActionOnly]
+        public ActionResult CartSummary()
+        {
+            var cart = ShoppingCart.GetCart(this.HttpContext);
+            ViewData["CartCount"] = cart.GetCount();
+            return PartialView("CartSummary");
+        }
+    }
+}
